936a1c824abfb1818ea4507878f42690ad02ab9b
isabel k. lee <hello@isabelklee.co>
2020-09-17 19:05:08 -0400

    fetched all books

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {})
+const booksURL = 'http://localhost:3000/books'
+const bookList = document.getElementById("list")
+
+fetch(booksURL)
+.then(r => r.json())
+.then((booksArr) => {
+  booksArr.forEach((book) => {
+    turnBookIntoLi(book)
+  })
+})
+
+let turnBookIntoLi = (book) => {
+  let bookLi = document.createElement("li")
+  bookLi.innerText = book.title
+  bookList.append(bookLi)
+}
+
